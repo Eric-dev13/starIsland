5aa0f703899a200bdf9d5a659da3094a75d71575
Eric-dev13 <81474009+Eric-dev13@users.noreply.github.com>
2023-07-12 18:51:01 +0200

    Update init.php

--- config/init.php
+++ config/init.php
@@ -22,3 +22,20 @@ const CONFIG=[
 
 const BASE_PATH='/';
 
+// localhost
+// const CONFIG=[
+//     'db'=>[
+//         'HOST'=>'localhost',
+//         'PORT'=>'3306',
+//         'NAME'=>'star_island',
+//         'USER'=>'root',
+//         'PWD'=>''
+//     ],
+//     'app'=>[
+//         'name'=>'starIsland',
+//         'projecturl'=>'http://localhost/starIsland'
+//     ]
+// ];
+
+// const BASE_PATH='/starIsland/';
+
